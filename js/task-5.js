5a95b53feaba873b42fb7857552d869c57177544
Anatoliy Don <anatoliy_don@MacBook-Air-Anatoliy.local>
2024-07-23 13:39:48 +0300

    try5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const coloredEl = document.querySelector('body');
+const textEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+
+const onBackGroundChange = () => {
+  coloredEl.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = `${getRandomHexColor()}`;
+}
+
+btnEl.addEventListener('click', onBackGroundChange);
