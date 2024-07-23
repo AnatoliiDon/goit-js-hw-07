dedb466b2f089e110d24808cc2c54d8dc6e6c9d2
Anatoliy Don <anatoliy_don@MacBook-Air-Anatoliy.local>
2024-07-23 13:17:26 +0300

    try4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+const onFormSubmit = event => {
+    event.preventDefault();
+
+    if (formEl.elements.email.value.trim().length === 0) {
+        alert('Invalid email value');
+    }
+    else if (formEl.elements.password.value.trim().length === 0) {
+      alert('Invalid password value');  
+    }
+    else {
+          const formData = {
+        email: formEl.elements.email.value.trim(),
+        password: formEl.elements.password.value.trim(),
+    };
+    console.log(formData);
+    }
+
+  
+}
+
+formEl.addEventListener('submit', onFormSubmit)
